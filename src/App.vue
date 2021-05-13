30ddb05968c5a5d41f33b11b721c055565d7f863
mae <myeongheeroh@gmail.com>
2021-05-13 16:01:26 +0900

    Counter : parent-child props ver

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+
+<template>
+    
+  <!-- App.vue(Parent) -->
+  <div id="app">
+    Parent counter : {{ counter }} <br>
+    <button @click="addCounter">+</button>
+    <button @click="subCounter">-</button>
+    <!-- Child 컴포넌트를 등록하고 counter 데이터 속성을 props로 전달한다. -->
+    <child v-bind:num="counter"></child>
+  </div>
+
+</template>
+
+<script>
+  import Child from "./Child.vue";
+
+  export default {
+    components: {
+      // Child 컴포넌트를 하위 컴포넌트로 등록
+      child: Child
+    },
+    data() {
+      return {
+        counter: 0
+      };
+    },
+    methods: {
+      // 이벤트 추가
+      addCounter() {
+        this.counter++;
+      },
+      subCounter() {
+        this.counter--;
+      }
+    }
+  };
+
+</script>
---
+++ src/Child.vue
@@ -0,0 +1,16 @@
+<template>
+    
+<!-- Child.vue(Child) -->
+<div>
+  <hr>
+  Child counter : {{ num }} <br>
+</div>
+
+</template>
+
+<script>
+export default {
+  // 상위 컴포넌트에서 내려준 counter 속성을 num로 받음
+  props: ["num"]
+}
+</script>
